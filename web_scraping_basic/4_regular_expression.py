542b5b63c909349ed23432e0104a2b38a75ef746
yjglab <yjgdesign@gmail.com>
2021-01-16 01:18:22 +0900

    regular expression

---
+++ web_scraping_basic/4_regular_expression.py
@@ -0,0 +1,39 @@
+import re
+# abcd, book, desk
+# ca?e
+# care, cafe, case, cave
+# caae, cabe, cace, cade, ...
+
+# 정규식 사용하기
+    # . => 하나의 문자    (ca.e) ex. care, cafe, case (o) | caffe (x)
+    # ^ => 문자열의 시작  (^de) ex. desk, destination (o) | fase (x)
+    # $ => 문자열의 끝    (se$) ex. case, base (o) | face (x)
+p = re.compile("ca.e") 
+
+def print_match(m):
+    if m: # 매치되었다면
+        print("m.group() : ", m.group()) # 일치하는 문자열을 반환
+        print("m.string : ", m.string) # 입력받은 문자열을 반환
+        print("m.start() : ", m.start()) # 일치하는 문자열의 시작 index 반환
+        print("m.end() : ", m.end()) # 일치하는 문자열의 끝 index 반환
+        print("m.span(): ", m.span()) # 일치하는 문자열의 시작 / 끝 index
+    else:
+        print("매칭되지 않음.")    
+
+# match() : 주어진 문자열의 처음부터 일치하는 지 확인
+m = p.match("case")
+print_match(m) # case => 정규식 p와 매치되어 출력됨. 매치되지 않으면 에러.
+m = p.match("good care")
+print_match(m) # 매칭되지 않음.
+m = p.match("careless") 
+print_match(m) # care
+
+# search() : 주어진 문자열 중에 일치하는 게 있는 지 확인
+m = p.search("good care") 
+print_match(m) # care
+
+# findall() : 일치하는 모든 것을 list 로 반환
+lst = p.findall("careless") 
+print(lst) # ['care']
+lst = p.findall("good care cafe") 
+print(lst) # ['care', 'cafe']
